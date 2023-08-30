eb811cc2c8bcc69eab244dbe1284bd9ac2affa0f
AliOding12 <aliz17043@gmail.com>
2023-08-30 10:00:00 +0500

    Add database configuration in database.php

---
+++ config/database.php
@@ -0,0 +1,6 @@
+<?php  
+//adding database script here 
+?>// Add database configuration in database.php
+// Add connection pooling configuration
+// Add database backup configuration
+// Add database configuration in database.php
